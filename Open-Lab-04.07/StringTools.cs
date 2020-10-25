ca724c60088315777d7b1b264558006c31e81188
Martin Kubica <martinko29.km@gmail.com>
2020-10-25 19:54:01 +0100

    +

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Open_Lab_04._07
 {
@@ -6,7 +8,20 @@ namespace Open_Lab_04._07
     {
         public string Reverse(string original)
         {
-            throw new NotImplementedException();
+            string a = "";
+          
+            char[] rg = new char[original.Length];
+            for (int i = 0; i < original.Length; i++)
+            {
+                rg[i] = original[i];
+                   
+            }
+            Array.Reverse(rg);
+
+              a = String.Join(a, rg);
+
+            return a;
+
         }
     }
 }
